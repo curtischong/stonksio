1fa55a99c88b3d1c1c24d0be896e65b816404bf4
Marcel O'Neil <marcel@marceloneil.com>
2021-09-19 01:33:33 -0400

    use connection pooling

--- server/pkg/database/cockroachDbClient.go
+++ server/pkg/database/cockroachDbClient.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"fmt"
+	"github.com/jackc/pgx/v4"
 	"log"
 	"stonksio/pkg/common"
 	"stonksio/pkg/config"
@@ -12,42 +13,41 @@ import (
 
 	"github.com/cockroachdb/cockroach-go/v2/crdb/crdbpgx"
 
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type CockroachDbClient struct {
 	config config.DatabaseConfig
-	conn   *pgx.Conn
+	db     *pgxpool.Pool
 }
 
 func NewCockroachDbClient(
 	config *config.Config,
 ) *CockroachDbClient {
 	// Connect to the stonksio database
-	connConfig, err := pgx.ParseConfig(config.DatabaseConfig.ConnectionString)
+	connConfig, err := pgxpool.ParseConfig(config.DatabaseConfig.ConnectionString)
 	if err != nil {
 		log.Fatal("error configuring the database: ", err)
 	}
 
-	connConfig.Database = "stonkst"
-	conn, err := pgx.ConnectConfig(context.Background(), connConfig)
+	conn, err := pgxpool.ConnectConfig(context.Background(), connConfig)
 	if err != nil {
 		log.Fatal("error connecting to the database: ", err)
 	}
 	return &CockroachDbClient{
 		config: config.DatabaseConfig,
-		conn:   conn,
+		db:     conn,
 	}
 }
 
 func (client *CockroachDbClient) closeConn() {
-	client.conn.Close(context.Background())
+	client.db.Close()
 }
 
 func (client *CockroachDbClient) InsertPost(
 	post common.Post,
 ) error {
-	return crdbpgx.ExecuteTx(context.Background(), client.conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
+	return crdbpgx.ExecuteTx(context.Background(), client.db, pgx.TxOptions{}, func(tx pgx.Tx) error {
 		log.Printf("Creating post=%s\n", post)
 		_, err := tx.Exec(context.Background(),
 			`INSERT INTO post (id, username, "userpicurl", "body", "timestamp") VALUES ($1, $2, $3, $4, $5)`,
@@ -57,7 +57,7 @@ func (client *CockroachDbClient) InsertPost(
 }
 
 func (client *CockroachDbClient) deleteAllPosts() error {
-	return crdbpgx.ExecuteTx(context.Background(), client.conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
+	return crdbpgx.ExecuteTx(context.Background(), client.db, pgx.TxOptions{}, func(tx pgx.Tx) error {
 		log.Printf("Deleting all posts")
 		_, err := tx.Exec(context.Background(), "DELETE FROM post")
 		return err
@@ -65,7 +65,7 @@ func (client *CockroachDbClient) deleteAllPosts() error {
 }
 
 func (client *CockroachDbClient) deleteAllPrices() error {
-	return crdbpgx.ExecuteTx(context.Background(), client.conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
+	return crdbpgx.ExecuteTx(context.Background(), client.db, pgx.TxOptions{}, func(tx pgx.Tx) error {
 		log.Printf("Deleting all prices")
 		_, err := tx.Exec(context.Background(), "DELETE FROM price")
 		return err
@@ -73,7 +73,7 @@ func (client *CockroachDbClient) deleteAllPrices() error {
 }
 
 func (client *CockroachDbClient) GetPosts(n int) ([]common.Post, error) {
-	rows, err := client.conn.Query(context.Background(),
+	rows, err := client.db.Query(context.Background(),
 		`SELECT id, username, userpicurl, body, timestamp FROM post ORDER BY timestamp DESC LIMIT $1;`, n)
 	if err != nil {
 		return nil, fmt.Errorf("cannot query rows. err=%s", err)
@@ -96,7 +96,7 @@ func (client *CockroachDbClient) GetPrices(
 	if asset != "ETH" {
 		return nil, fmt.Errorf("invalid asset=%s", asset)
 	}
-	rows, err := client.conn.Query(context.Background(),
+	rows, err := client.db.Query(context.Background(),
 		`SELECT tradePrice, timestamp FROM price WHERE asset=$1 ORDER BY timestamp DESC LIMIT $2;`, asset, n)
 	if err != nil {
 		return nil, fmt.Errorf("cannot query prices. err=%s", err)
@@ -122,7 +122,7 @@ func (client *CockroachDbClient) GetLatestPrice(
 		return 0, fmt.Errorf("invalid asset=%s", asset)
 	}
 
-	rows, err := client.conn.Query(context.Background(),
+	rows, err := client.db.Query(context.Background(),
 		"SELECT tradePrice FROM price ORDER BY timestamp DESC LIMIT 1")
 	if err != nil {
 		return 0, err
@@ -140,7 +140,7 @@ func (client *CockroachDbClient) InsertPrice(
 	if asset != "ETH" {
 		return fmt.Errorf("invalid asset=%s", asset)
 	}
-	return crdbpgx.ExecuteTx(context.Background(), client.conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
+	return crdbpgx.ExecuteTx(context.Background(), client.db, pgx.TxOptions{}, func(tx pgx.Tx) error {
 		log.Printf("Creating tradePrice=%f for asset=%s\n", tradePrice, asset)
 		_, err := tx.Exec(context.Background(),
 			"INSERT INTO price (id, asset, tradePrice, timestamp) VALUES ($1, $2, $3, $4)",
