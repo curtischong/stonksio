747e585c7afacd798c10de0f9adc15c6ec39130e
Marcel O'Neil <marcel@marceloneil.com>
2021-09-19 07:45:27 -0400

    pusher logging

--- server/pkg/websocket/pusherApiClient.go
+++ server/pkg/websocket/pusherApiClient.go
@@ -3,10 +3,13 @@ package websocket
 import (
 	pusher "github.com/pusher/pusher-http-go"
 	"stonksio/pkg/common"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // the file can't be called pusherClient to for syntax highlighting to work for some reason
 type PusherClient struct {
+	logger       *log.Logger
 	socketClient pusher.Client
 }
 
@@ -19,6 +22,7 @@ func NewPusherClient() *PusherClient {
 		Secure:  true,
 	}
 	return &PusherClient{
+		logger:       log.New(),
 		socketClient: socketClient,
 	}
 }
@@ -26,11 +30,17 @@ func NewPusherClient() *PusherClient {
 func (client *PusherClient) PushPost(
 	post *common.Post,
 ) {
-	client.socketClient.Trigger("post", "new-post", post)
+	client.logger.Infof("pushing post %s", post.Id)
+	if err := client.socketClient.Trigger("post", "new-post", post); err != nil {
+		client.logger.Errorf("could not push post, err=%s", err)
+	}
 }
 
 func (client *PusherClient) PushPrice(
 	price *common.Price,
 ) {
-	client.socketClient.Trigger("prices", "new-price", price)
+	client.logger.Infof("pushing price %f", price.TradePrice)
+	if err := client.socketClient.Trigger("prices", "new-price", price); err != nil {
+		client.logger.Errorf("could not push price, err=%s", err)
+	}
 }
